dda3cafa156d9cc6f25ecf88f023337f4f22fe85
BARIŞ COŞKUN <bariscoskun441@gmail.com>
2026-06-14 00:43:14 +0300

    feat: add role management features and user role assignment backend

---
+++ NotikaIdentityEmail/Controllers/RoleController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> RoleList()
+        {
+            var values = await _roleManager.Roles.ToListAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            IdentityRole role = new IdentityRole()
+            {
+                Name = model.RoleName
+            };
+            await _roleManager.CreateAsync(role);
+
+
+            return RedirectToAction("RoleList");
+        }
+
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var value = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            await _roleManager.DeleteAsync(value);
+            return RedirectToAction("RoleList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRole(string id)
+        {
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel()
+            {
+                RoleId = role.Id,
+                RoleName = role.Name
+            };
+            return View(updateRoleViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(UpdateRoleViewModel model)
+        {
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == model.RoleId);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            role.Name = model.RoleName;
+            await _roleManager.UpdateAsync(role);
+            return RedirectToAction("RoleList");
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(string id)
+        { 
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            TempData["UserId"] = user.Id;
+            var roles = await _roleManager.Roles.ToListAsync();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles) 
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExists = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        { 
+            var userId = TempData["UserId"].ToString();
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            foreach (var item in model) {
+                if (item.RoleExists)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else 
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("UserList");
+        }
+       
+    }
+}
---
+++ NotikaIdentityEmail/Models/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NotikaIdentityEmail.Models
+{
+    public class RoleAssignViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool RoleExists { get; set; }
+    }
+}
